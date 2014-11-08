8ced394911e41d550f922671daa41422ba223fbb
J15t98J <J15t98J@gmail.com>
2014-11-08 00:55:27 +0000

    Non-functional login screen

---
+++ mobile/src/main/java/uk/herts/sch/damealiceowens/studio/daos/LoginActivity.java
@@ -0,0 +1,55 @@
+package uk.herts.sch.damealiceowens.studio.daos;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.EditText;
+
+import uk.sch.herts.damealiceowens.studio.daos.R;
+
+public class LoginActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.login_activity);
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.login_activity, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    // GUI FUNCTIONS
+    public boolean validate(String uname, String pword) {
+        // return DB query
+        return true; // test purposes
+    }
+
+
+    public void login(View view) {
+        if(validate((EditText)findViewById(R.id.uname))) { // no .getText?!?
+
+        }
+    }
+}
