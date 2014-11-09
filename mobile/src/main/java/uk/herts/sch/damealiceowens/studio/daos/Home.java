8e9f33aa9ba3c14b4337e5d84ba2e0229ff45195
zacdevil10 <zac1999ad@gmail.com>
2014-11-09 20:55:39 +0000

    Action bar has values and opens fragments (tests)

--- mobile/src/main/java/uk/herts/sch/damealiceowens/studio/daos/Home.java
+++ mobile/src/main/java/uk/herts/sch/damealiceowens/studio/daos/Home.java
@@ -1,33 +1,93 @@
 package uk.herts.sch.damealiceowens.studio.daos;
 
 import android.app.Activity;
+import android.app.Fragment;
+import android.app.FragmentManager;
 import android.os.Bundle;
+import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
+import android.support.v7.internal.widget.AdapterViewCompat;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Adapter;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 
 import uk.sch.herts.damealiceowens.studio.daos.R;
+import uk.sch.herts.damealiceowens.studio.daos.TEST1;
+import uk.sch.herts.damealiceowens.studio.daos.TEST2;
+
+import static android.support.v7.internal.widget.AdapterViewCompat.*;
 
 
 public class Home extends Activity {
-    private String[] Items;
     private DrawerLayout mDrawerLayout;
     private ListView mDrawerList;
+    private ActionBarDrawerToggle mDrawerToggle;
+    private CharSequence mDrawerTitle;
+    private CharSequence mTitle;
+    private String[] mItems;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
 
-        //Items = getResources().getStringArray(R.array.Items);
+        mTitle = getTitle();
+        mItems = getResources().getStringArray(R.array.Items);
         mDrawerLayout = (DrawerLayout) findViewById(R.id.drawer_layout);
         mDrawerList = (ListView) findViewById(R.id.left_drawer);
 
-        //mDrawerList.setAdapter(new ArrayAdapter<String>(this, R.layout.));
+        mDrawerList.setAdapter(new ArrayAdapter<String>(this, R.layout.drawer_list_item, mItems));
 
 
+        mDrawerList.setOnItemClickListener(new DrawerItemClickListener());
+
+        getActionBar().setDisplayHomeAsUpEnabled(true);
+        getActionBar().setHomeButtonEnabled(true);
+
+        mDrawerToggle = new ActionBarDrawerToggle(
+                this,
+                mDrawerLayout,
+                R.string.drawer_open,
+                R.string.drawer_close
+        ){
+
+            public void onDrawerClosed(View view){
+                super.onDrawerClosed(view);
+                getActionBar().setTitle(mTitle);
+                invalidateOptionsMenu();
+            }
+
+            public void onDrawerOpened(View drawerView) {
+                super.onDrawerOpened(drawerView);
+                getActionBar().setTitle("Dame Alice Owen's");
+                invalidateOptionsMenu();
+            }
+
+        };
+
+        mDrawerLayout.setDrawerListener(mDrawerToggle);
+
+        //String[] values = new String[]{"Home", "News and Events", "Timetable", "Homework"};
+
+        //ArrayAdapter<String>adapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_activated_1, android.R.id.text1, values);
+
+        //mDrawerList.setAdapter(adapter);
+
+        if (savedInstanceState == null){
+            selectItem(0);
+        }
+
+    }
+
+    public boolean onPrepareOptionsMenu(Menu menu){
+        boolean drawerOpen = mDrawerLayout.isDrawerOpen(mDrawerList);
+        //menu.findItem().setVisible(!drawerOpen);
+        return super.onPrepareOptionsMenu(menu);
     }
 
 
@@ -52,4 +112,36 @@ public class Home extends Activity {
 
         return super.onOptionsItemSelected(item);
     }
+
+    private class DrawerItemClickListener implements ListView.OnItemClickListener{
+        @Override
+        public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+            selectItem(position);
+        }
+    }
+
+    private void selectItem(int position){
+
+        Fragment fragment1 = new TEST1();
+        Fragment fragment2 = new TEST2();
+        FragmentManager fragmentManager = getFragmentManager();
+        //fragmentManager.beginTransaction().replace(R.id.content_frame, fragment).commit();
+
+        if (position == 1){
+            fragmentManager.beginTransaction().replace(R.id.content_frame, fragment1).commit();
+        } if (position == 2){
+            fragmentManager.beginTransaction().replace(R.id.content_frame, fragment2).commit();
+        }
+
+        mDrawerList.setItemChecked(position, true);
+        setTitle(mItems[position]);
+        mDrawerLayout.closeDrawer(mDrawerList);
+    }
+
+    public void setTitle(CharSequence title){
+        mTitle = title;
+        getActionBar().setTitle(mTitle);
+    }
 }
+
+
