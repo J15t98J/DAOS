df86cfb9bbd9addcac45e3dd35bf1d04fa1ce2dc
zacdevil10 <zac1999ad@gmail.com>
2015-01-14 20:28:00 +0000

    Redone

---
+++ app/src/main/java/uk/co/appsbystudio/damealiceowens/Pages/pagecontentviews/NewsList.java
@@ -0,0 +1,33 @@
+package uk.co.appsbystudio.damealiceowens.Pages.pagecontentviews;
+
+
+import android.os.Bundle;
+import android.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import uk.co.appsbystudio.damealiceowens.R;
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class NewsList extends Fragment {
+
+
+    public NewsList() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_news_list, container, false);
+    }
+
+
+}
