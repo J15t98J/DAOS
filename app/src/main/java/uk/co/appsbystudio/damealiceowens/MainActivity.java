ee8a0b0669280f643ebc4caddcc2ddaac4383541
J15t98J <J15t98J@gmail.com>
2015-02-04 22:15:26 +0000

    Optimised fragment switching

--- app/src/main/java/uk/co/appsbystudio/damealiceowens/MainActivity.java
+++ app/src/main/java/uk/co/appsbystudio/damealiceowens/MainActivity.java
@@ -4,7 +4,7 @@ import android.app.Fragment;
 import android.app.FragmentManager;
 import android.content.res.Configuration;
 import android.os.Bundle;
-import android.support.v4.app.ActionBarDrawerToggle;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarActivity;
@@ -14,6 +14,10 @@ import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
+
+import java.util.HashMap;
+import java.util.Map;
+
 import uk.co.appsbystudio.damealiceowens.Pages.Home;
 import uk.co.appsbystudio.damealiceowens.Pages.News;
 import uk.co.appsbystudio.damealiceowens.Pages.Planner;
@@ -41,7 +45,7 @@ public class MainActivity extends ActionBarActivity  {
         items = getResources().getStringArray(R.array.main_items);
         drawerLayout = (DrawerLayout) findViewById(R.id.drawer_layout);
         drawerLayout.setDrawerShadow(R.drawable.drawer_shadow, GravityCompat.START);
-        drawerToggle = new ActionBarDrawerToggle(this, drawerLayout, R.drawable.ic_drawer, R.string.drawer_open, R.string.drawer_close) {
+        drawerToggle = new ActionBarDrawerToggle(this, drawerLayout, R.string.drawer_open, R.string.drawer_close) {
             public void onDrawerClosed(View view) {
                 super.onDrawerClosed(view);
                 getSupportActionBar().setTitle(title);
@@ -59,15 +63,13 @@ public class MainActivity extends ActionBarActivity  {
 
         listView = (ListView) findViewById(R.id.left_drawer);
 
-        listView.setAdapter(new ArrayAdapter<String>(this, R.layout.drawer_list_item, items));
+        listView.setAdapter(new ArrayAdapter<>(this, R.layout.drawer_list_item, items));
         listView.setSelection(0);
 
         listView.setOnItemClickListener(new DrawerItemCLickListener());
 
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
         getSupportActionBar().setHomeButtonEnabled(true);
-
-
     }
 
     @Override
@@ -82,81 +84,38 @@ public class MainActivity extends ActionBarActivity  {
         drawerToggle.onConfigurationChanged(newConfig);
     }
 
-
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.menu_main, menu);
         return true;
     }
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        if (drawerToggle.onOptionsItemSelected(item)) {
-            return true;
-        }
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
+	    return drawerToggle.onOptionsItemSelected(item) || item.getItemId() == R.id.action_settings || super.onOptionsItemSelected(item);
     }
 
     public void onBackPressed() {
         if (drawerLayout.isDrawerOpen(listView)) {
             drawerLayout.closeDrawer(listView);
-        }else {
+        } else {
             super.onBackPressed();
         }
     }
 
-
     public class DrawerItemCLickListener implements ListView.OnItemClickListener {
         @Override
         public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-            selectItem(position);
-        }
+	        Map<Integer, Fragment> fragments = new HashMap<>();
+	        fragments.put(0, new Home());
+	        fragments.put(1, new News());
+	        fragments.put(2, new Planner());
 
-        private void selectItem(int position) {
-            Fragment fragment0 = new Home();
-            Fragment fragment1 = new News();
-            Fragment fragment2 = new Planner();
+	        getFragmentManager().beginTransaction().replace(R.id.content_frame, fragments.get(position)).addToBackStack(null).commit();
 
-            FragmentManager fragmentManager = getFragmentManager();
-
-            if (position == 0) {
-                fragmentManager
-                        .beginTransaction()
-                        .replace(R.id.content_frame, fragment0)
-                        .addToBackStack(null)
-                        .commit();
-            }
-            if (position == 1) {
-                fragmentManager
-                        .beginTransaction()
-                        .replace(R.id.content_frame, fragment1)
-                        .addToBackStack(null)
-                        .commit();
-            }
-            if (position == 2) {
-                fragmentManager
-                        .beginTransaction()
-                        .replace(R.id.content_frame, fragment2)
-                        .addToBackStack(null)
-                        .commit();
-            }
-
-            listView.setItemChecked(position, true);
-            setTitle(items[position]);
-            drawerLayout.closeDrawer(listView);
+	        listView.setItemChecked(position, true);
+	        setTitle(items[position]);
+	        drawerLayout.closeDrawer(listView);
         }
     }
-
 }
