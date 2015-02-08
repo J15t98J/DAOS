c50bffb1a08486f63da550452b5865139e79eeab
J15t98J <J15t98J@gmail.com>
2015-02-08 23:12:53 +0000

    Finished feed parser implementation but address isn't recognised

--- app/src/main/java/uk/co/appsbystudio/damealiceowens/util/RSSFeedParser.java
+++ app/src/main/java/uk/co/appsbystudio/damealiceowens/util/RSSFeedParser.java
@@ -2,16 +2,30 @@ package uk.co.appsbystudio.damealiceowens.util;
 
 import android.os.AsyncTask;
 
+import org.xmlpull.v1.XmlPullParser;
+import org.xmlpull.v1.XmlPullParserException;
+import org.xmlpull.v1.XmlPullParserFactory;
+
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.net.MalformedURLException;
+import java.io.StringReader;
 import java.net.URL;
 import java.util.ArrayList;
 
 public class RSSFeedParser extends AsyncTask<String, Void, ArrayList<RSSItem>> {
 
 	private ArrayList<RSSItem> array = new ArrayList<RSSItem>();
+	private ArrayList<String> accepted_tags = new ArrayList<String>();
+
+	public RSSFeedParser() {
+		accepted_tags.add(0, "item");
+		accepted_tags.add(1, "title");
+		accepted_tags.add(2, "pubDate");
+		accepted_tags.add(3, "author");
+		accepted_tags.add(4, "description");
+		accepted_tags.add(5, "guid");
+	}
 
 	@Override
 	protected ArrayList<RSSItem> doInBackground(String... params) {
@@ -26,11 +40,42 @@ public class RSSFeedParser extends AsyncTask<String, Void, ArrayList<RSSItem>> {
 					data += line;
 				}
 
-				// Loop that creates a new RSSItem every time an <item> is found
-				// If following open tags are ones we want, format them and put them in the RSItem HashMap
-				// On </item> put the RSSItem into the ArrayList
+				XmlPullParserFactory factory = XmlPullParserFactory.newInstance();
+				factory.setNamespaceAware(true);
+				XmlPullParser parser = factory.newPullParser();
 
-			} catch(IOException e) {
+				parser.setInput(new StringReader(data));
+
+				int event;
+				RSSItem currentItem = new RSSItem();
+				String currentTag = "";
+				String currentValue = "";
+
+				while((event = parser.getEventType()) != XmlPullParser.END_DOCUMENT) {
+					if(event == XmlPullParser.START_TAG) {
+						if(currentTag.equals("") && accepted_tags.contains(parser.getName())) {
+							if(parser.getName().equals("item")) {
+								currentItem = new RSSItem();
+							}
+							currentTag = parser.getName();
+						} else {
+							currentValue += "<" + parser.getName() + ">";
+						}
+					} else if(event == XmlPullParser.END_TAG) {
+						if(currentTag.equals(parser.getName())) {
+							if(parser.getName().equals("item")) {
+								array.add(currentItem);
+							}
+							currentItem.setValue(currentTag, currentValue);
+							currentTag = "";
+							currentValue = "";
+						} else {
+							currentValue += "</" + parser.getName() + ">";
+						}
+					}
+				}
+
+			} catch(IOException | XmlPullParserException e) {
 				e.printStackTrace();
 			}
 		}
